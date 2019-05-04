1c08c1eac26e734af457e975be36fc1593234964
EmmanuelLopez1 <maximino33@hotmail.com>
2019-05-04 00:14:26 -0500

    se creo la version beta de la pagina comics

---
+++ src/app/novela/novela.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector : 'novela-comic',
+    templateUrl : 'novela.component.html',
+    styleUrls : ['novela.component.css']
+})
+
+export class NovelaComponent{
+    
+}
