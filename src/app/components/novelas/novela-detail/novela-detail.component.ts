4a8a45fab569d768b042ac50d4cfd7696775d164
Diego Mosan <diego14290764@itcg.edu.mx>
2019-05-11 13:34:13 -0500

    The cards was added and the novels component was added.

---
+++ src/app/components/novelas/novela-detail/novela-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-novela-detail',
+  templateUrl: './novela-detail.component.html',
+  styleUrls: ['./novela-detail.component.sass']
+})
+export class NovelaDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shared/components/carrousel/carrousel.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-corrousel',
+    templateUrl: './carrousel.component.html',
+    styleUrls: ['./carrousel.component.css']
+})
+export class CarrouselComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void { }
+}
